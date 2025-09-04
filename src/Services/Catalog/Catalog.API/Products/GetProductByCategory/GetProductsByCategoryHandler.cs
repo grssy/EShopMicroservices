7226acc86998d5729c2e90dd41d192683388e707
Gabriel Vieira <gabrielvieirasousa2001@hotmail.com>
2025-09-03 23:03:53 -0300

    creating catalog api with cqrs and vertical slice architecture

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+public class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryQueryHandler.Hanlde called with {@Query}", query);
+
+        var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync(cancellationToken);
+
+        return new GetProductsByCategoryResult(products);
+    }
+}
