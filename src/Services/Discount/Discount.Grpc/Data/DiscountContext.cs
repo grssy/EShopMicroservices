15fdca092cc9ffdfa6ed85c4a69322051ba0aab9
Gabriel Vieira <gabrielvieirasousa2001@hotmail.com>
2025-09-06 16:06:12 -0300

    addding discount section

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount value", Amount = 123 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount value", Amount = 120 }
+            );
+    }
+}
